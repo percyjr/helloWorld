4f48a97597d9f162cd969b3e1378a44f0c60dc86
percy <percy@PercyXPS.lan>
2020-12-25 21:57:47 -0600

    First commit

---
+++ HelloWorld/src/hello/Hello.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int y = 10;
+		short x = (short)y;
+		System.out.println(y);
+		
+	}
+
+}
